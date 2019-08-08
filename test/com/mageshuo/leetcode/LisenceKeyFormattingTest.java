b6d4e3701f0022ecb63d8d663233ee50081f06ea
Ma, Jason <jason.ma@activenetwork.com>
2019-08-08 14:40:33 +0800

    Licence Key Formatting.

---
+++ test/com/mageshuo/leetcode/LisenceKeyFormattingTest.java
@@ -0,0 +1,19 @@
+package com.mageshuo.leetcode;
+
+
+public class LisenceKeyFormattingTest {
+    public static void main(String[] args) {
+        String result;
+        LisenceKeyFormatting formatting = new LisenceKeyFormatting();
+        String string1 = "5F3Z-2e-9-w";
+        int k1 = 4;
+        result = formatting.licenseKeyFormatting(string1, k1);
+        assert result == "5F3Z-2E9W" : "Not right";
+        System.out.println(result);
+        String string2 = "2-5g-3-J";
+        int k2 = 2;
+        result = formatting.licenseKeyFormatting(string2, k2);
+        assert result == "2-5G-3J" : "Not cool";
+        System.out.println(result);
+    }
+}
